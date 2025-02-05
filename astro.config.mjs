3b80d1e05bdd32b2e28c7c9935b5da902a724ede
MateoLDC <leducmateo@gmail.com>
2025-02-05 19:46:33 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: "PotesCafe-Links",
+    integrations: [icon()],
+});
